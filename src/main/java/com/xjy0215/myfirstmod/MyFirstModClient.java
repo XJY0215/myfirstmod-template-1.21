c2ec93f6705747d5db2535558f3f2da4a3804f16
Junyi Xiao <3653124057@qq.com>
2025-07-28 15:30:18 +0800

    提交

---
+++ src/main/java/com/xjy0215/myfirstmod/MyFirstModClient.java
@@ -0,0 +1,10 @@
+package com.xjy0215.myfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
