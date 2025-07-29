f52da0d14f451379338a26c607b978162a55d3b4
XJY0215 <3653124057@qq.com>
2025-07-29 21:03:34 +0800

    提交

--- src/main/java/com/xjy0215/myfirstmod/item/ModItems.java
+++ src/main/java/com/xjy0215/myfirstmod/item/ModItems.java
@@ -12,7 +12,7 @@ import static net.minecraft.item.Items.register;
 public class ModItems {
     public static final Item Ice_ETHER = register("ice_ether", new Item(new Item.Settings()));
     private static Item registerItems(String id, Item item){
-        return return Registry.register(Registries.ITEM, Identifier.of(MyFirstMod.MOD_ID, id), item);
+        return Registry.register(Registries.ITEM, Identifier.of(MyFirstMod.MOD_ID, id), item);
     }
     public static void registerModItems(){
         MyFirstMod.LOGGER.info("Registering Items");
